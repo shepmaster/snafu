86fc67e3e79dd349abc17750d73686a3e4f9ed1d
Jake Goulding <jake.goulding@gmail.com>
2019-01-27 19:33:05 -0500

    Add a lazier `with_context`

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,19 @@ pub struct Context<E, C> {
 
 pub trait ResultExt<T, E> {
     fn context<C>(self, context: C) -> Result<T, Context<E, C>>;
+
+    fn with_context<C>(self, context: impl FnOnce() -> C) -> Result<T, Context<E, C>>;
 }
 
 impl<T, E> ResultExt<T, E> for std::result::Result<T, E> {
     fn context<C>(self, context: C) -> Result<T, Context<E, C>> {
         self.map_err(|error| Context { error, context })
+    }
+
+    fn with_context<C>(self, context: impl FnOnce() -> C) -> Result<T, Context<E, C>> {
+        self.map_err(|error| {
+            let context = context();
+            Context { error, context }
+        })
     }
 }
