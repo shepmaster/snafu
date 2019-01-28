2e657ccb8981c9eb22e94c0b7858e0079ebd9e30
Jake Goulding <jake.goulding@gmail.com>
2019-01-27 22:00:25 -0500

    Test the unstable attributes parsing

---
+++ tests/unstable_attributes.rs
@@ -0,0 +1,40 @@
+#![feature(unrestricted_attribute_tokens)]
+
+use snafu::{Snafu, ResultExt};
+use std::{fs, io, path::{Path, PathBuf}};
+
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu::display("Could not open config file at {}: {}", filename.display(), source)]
+    OpenConfig { filename: PathBuf, source: io::Error },
+    #[snafu::display("Could not open config file at {}", source)]
+    SaveConfig { source: io::Error },
+    #[snafu::display("No user available")]
+    MissingUser,
+}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+const CONFIG_FILENAME: &str = "/tmp/config";
+
+fn example(root: impl AsRef<Path>, username: &str) -> Result<()> {
+    let root = root.as_ref();
+    let filename = &root.join(CONFIG_FILENAME);
+
+    let config = fs::read(filename).context(OpenConfig { filename })?;
+
+    if username.is_empty() {
+        return Err(Error::MissingUser);
+    }
+
+    fs::write(filename, config).context(SaveConfig)?;
+
+    Ok(())
+}
+
+#[test]
+fn implements_error() {
+    fn check<T: std::error::Error>() {}
+    check::<Error>();
+    example("/some/directory/that/does/not/exist", "").unwrap_err();
+}
