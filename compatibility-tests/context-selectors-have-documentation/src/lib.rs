13a9720f66071184d1c6e6503c8865fdc34baf01
R.Chavignat <r.chavignat@gmail.com>
2019-09-23 19:25:51 +0200

    Generate documentation for context selectors
    Add `#[allow(missing_docs)]` attribute to the selector fields.
    
    Closes #168

---
+++ compatibility-tests/context-selectors-have-documentation/src/lib.rs
@@ -0,0 +1,13 @@
+#![deny(missing_docs)]
+
+//! Crate docs
+
+use snafu::Snafu;
+
+#[derive(Debug, Snafu)]
+#[snafu(visibility = "pub")]
+/// Enum docs
+pub enum Error {
+    /// Variant docs
+    Variant,
+}
