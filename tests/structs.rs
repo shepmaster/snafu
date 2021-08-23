dc3ed3d1c9ef34459eaf368a3f346447712e0ea7
Jake Goulding <jake.goulding@gmail.com>
2021-08-23 10:26:56 -0400

    Revert "No need to work around rustfmt issue anymore"
    Turns out that rustfmt was *silently* erroring, not actually fixed.
    
    This reverts commit 30cd25a2a72d71ba10fff8fc86ea56fb8c8d56d9.

--- tests/structs.rs
+++ tests/structs.rs
@@ -1,14 +1,27 @@
-mod structs {
-    mod backtrace;
-    mod backtrace_attributes;
-    mod context_selector_name;
-    mod display;
-    mod from_option;
-    mod generics;
-    mod no_context;
-    mod single_use_lifetimes;
-    mod source_attributes;
-    mod visibility;
-    mod with_source;
-    mod without_source;
-}
+// Using #[path] to work around a https://github.com/rust-lang/rustfmt/issues/4404
+// Once fixed and released, switch to a `mod structs { ... }`
+
+#[path = "structs/backtrace.rs"]
+mod backtrace;
+#[path = "structs/backtrace_attributes.rs"]
+mod backtrace_attributes;
+#[path = "structs/context_selector_name.rs"]
+mod context_selector_name;
+#[path = "structs/display.rs"]
+mod display;
+#[path = "structs/from_option.rs"]
+mod from_option;
+#[path = "structs/generics.rs"]
+mod generics;
+#[path = "structs/no_context.rs"]
+mod no_context;
+#[path = "structs/single_use_lifetimes.rs"]
+mod single_use_lifetimes;
+#[path = "structs/source_attributes.rs"]
+mod source_attributes;
+#[path = "structs/visibility.rs"]
+mod visibility;
+#[path = "structs/with_source.rs"]
+mod with_source;
+#[path = "structs/without_source.rs"]
+mod without_source;
