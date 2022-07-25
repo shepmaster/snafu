6ca4bbcb684bdc75b1d74d1541b0d3e7aab670f6
OliverUv <oliver.uvman@gmail.com>
2022-07-25 20:50:32 +0200

    Allow iter-chain to be used on no_std

--- src/lib.rs
+++ src/lib.rs
@@ -252,9 +252,7 @@ pub use std::backtrace::Backtrace;
 #[cfg(feature = "futures")]
 pub mod futures;
 
-#[cfg(feature = "std")]
 mod error_chain;
-#[cfg(feature = "std")]
 pub use crate::error_chain::*;
 
 doc_comment::doc_comment! {
@@ -969,7 +967,6 @@ pub trait ErrorCompat {
     ///
     /// To omit the current error and only traverse its sources,
     /// use `skip(1)`.
-    #[cfg(feature = "std")]
     fn iter_chain(&self) -> ChainCompat
     where
         Self: AsErrorSource,
