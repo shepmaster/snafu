3335331f1e0d9a254e0f13fb93a71ed29f8319a9
Jake Goulding <jake.goulding@gmail.com>
2024-07-08 17:23:21 -0400

    error-in-core is stabilized in 1.81 (nightly)
    Allow the library to continue to enable the feature even though it's
    inert, allowing people to continue using this version of SNAFU with
    older nightly versions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![deny(missing_docs)]
+#![allow(stable_features)]
 #![cfg_attr(not(any(feature = "std", test)), no_std)]
 #![cfg_attr(feature = "unstable-core-error", feature(error_in_core))]
 #![cfg_attr(
