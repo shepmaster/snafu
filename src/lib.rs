776a6b8961ef0717e217db69cb2980d31e7af647
Jake Goulding <jake.goulding@gmail.com>
2025-12-03 12:23:49 -0500

    Extract `Whatever` and `WhateverLocal` to new file

--- src/lib.rs
+++ src/lib.rs
@@ -446,6 +446,11 @@ pub use fallback_error::Error;
 #[cfg(any(feature = "alloc", test))]
 mod boxed_impls;
 
+#[cfg(any(feature = "alloc", test))]
+mod whatever;
+#[cfg(any(feature = "alloc", test))]
+pub use whatever::*;
+
 /// Ensure a condition is true. If it is not, return from the function
 /// with an error.
 ///
@@ -1590,118 +1595,6 @@ macro_rules! location {
     () => {
         $crate::Location::new(file!(), line!(), column!())
     };
-}
-
-/// A basic error type that you can use as a first step to better
-/// error handling.
-///
-/// You can use this type in your own application as a quick way to
-/// create errors or add basic context to another error. This can also
-/// be used in a library, but consider wrapping it in an
-/// [opaque](guide::opaque) error to avoid putting the SNAFU crate in
-/// your public API.
-///
-/// ## Examples
-///
-/// ```rust
-/// use snafu::prelude::*;
-///
-/// type Result<T, E = snafu::Whatever> = std::result::Result<T, E>;
-///
-/// fn subtract_numbers(a: u32, b: u32) -> Result<u32> {
-///     if a > b {
-///         Ok(a - b)
-///     } else {
-///         whatever!("Can't subtract {a} - {b}")
-///     }
-/// }
-///
-/// fn complicated_math(a: u32, b: u32) -> Result<u32> {
-///     let val = subtract_numbers(a, b).whatever_context("Can't do the math")?;
-///     Ok(val * 2)
-/// }
-/// ```
-///
-/// See [`whatever!`][] for detailed usage instructions.
-///
-/// ## Limitations
-///
-/// When wrapping errors, only the backtrace from the shallowest
-/// function is guaranteed to be available. If you need the deepest
-/// possible trace, consider creating a custom error type and [using
-/// `#[snafu(backtrace)]` on the `source`
-/// field](Snafu#controlling-backtraces). If a best-effort attempt is
-/// sufficient, see the [`backtrace`][Self::backtrace] method.
-///
-/// When the standard library stabilizes support for the
-/// [provide API](https://doc.rust-lang.org/std/error/trait.Error.html#method.provide),
-/// this behavior may change.
-///
-/// ## Thread Safety
-///
-/// This type requires that contained errors implement [`Send`][] and
-/// [`Sync`][]. If this is burdensome, you may also use
-/// [`WhateverLocal`][].
-#[derive(Debug, Snafu)]
-#[snafu(crate_root(crate))]
-#[snafu(whatever)]
-#[snafu(display("{message}"))]
-#[snafu(provide(opt, ref, chain, dyn crate::Error + Send + Sync => source.as_deref()))]
-#[cfg(any(feature = "alloc", test))]
-pub struct Whatever {
-    #[snafu(source(from(Box<dyn crate::Error + Send + Sync>, Some)))]
-    #[snafu(provide(false))]
-    source: Option<Box<dyn crate::Error + Send + Sync>>,
-    message: String,
-    backtrace: Backtrace,
-}
-
-#[cfg(any(feature = "alloc", test))]
-impl Whatever {
-    /// Gets the backtrace from the deepest `Whatever` error. If none
-    /// of the underlying errors are `Whatever`, returns the backtrace
-    /// from when this instance was created.
-    pub fn backtrace(&self) -> Option<&Backtrace> {
-        ChainCompat::new(self)
-            .filter_map(|e| e.downcast_ref::<Self>())
-            .last()
-            .map(|w| &w.backtrace)
-    }
-}
-
-/// A basic error type that you can use as a first step to better
-/// error handling when the error does not need to cross a thread
-/// boundary.
-///
-/// This type behaves the same as [`Whatever`][] except it does not
-/// require that the wrapped errors implement [`Send`][] or
-/// [`Sync`][]. See [`Whatever`][] and [`whatever!`][] for detailed
-/// usage instructions.
-#[derive(Debug, Snafu)]
-#[snafu(crate_root(crate))]
-#[snafu(whatever)]
-#[snafu(display("{message}"))]
-#[snafu(provide(opt, ref, chain, dyn crate::Error => source.as_deref()))]
-#[cfg(any(feature = "alloc", test))]
-pub struct WhateverLocal {
-    #[snafu(source(from(Box<dyn crate::Error>, Some)))]
-    #[snafu(provide(false))]
-    source: Option<Box<dyn crate::Error>>,
-    message: String,
-    backtrace: Backtrace,
-}
-
-#[cfg(any(feature = "alloc", test))]
-impl WhateverLocal {
-    /// Gets the backtrace from the deepest `WhateverLocal` error. If
-    /// none of the underlying errors are `WhateverLocal`, returns the
-    /// backtrace from when this instance was created.
-    pub fn backtrace(&self) -> Option<&Backtrace> {
-        ChainCompat::new(self)
-            .filter_map(|e| e.downcast_ref::<Self>())
-            .last()
-            .map(|w| &w.backtrace)
-    }
 }
 
 mod tests {
