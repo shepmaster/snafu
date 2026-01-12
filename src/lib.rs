c40fbc1546d1b9dd5f7ec88ab63779fe76f956c3
Jake Goulding <jake.goulding@gmail.com>
2026-01-12 11:58:23 -0500

    Use the standard library location type
    Closes #546

--- src/lib.rs
+++ src/lib.rs
@@ -264,8 +264,6 @@
 //! capabilities, then read the [user's guide](guide) for deeper
 //! understanding.
 
-use core::fmt;
-
 #[cfg(feature = "alloc")]
 extern crate alloc;
 #[cfg(feature = "alloc")]
@@ -1470,8 +1468,8 @@ fn backtrace_collection_enabled() -> bool {
 /// combinator's library.
 ///
 /// There are two workarounds:
-/// 1. Use the [`location!`] macro
-/// 1. Use [`ResultExt`] instead
+/// 1. Avoid combinators and use the non-async [`ResultExt`]
+/// 1. Construct the location explicitly, such as by the [`location!`] macro
 ///
 /// ```rust
 /// # #[cfg(feature = "futures")] {
@@ -1495,73 +1493,23 @@ fn backtrace_collection_enabled() -> bool {
 /// struct ImplicitLocationError {
 ///     source: AnotherError,
 ///     #[snafu(implicit)]
-///     location: Location,
+///     location: snafu::Location,
 /// }
 ///
 /// #[derive(Debug, Snafu)]
 /// struct ExplicitLocationError {
 ///     source: AnotherError,
-///     location: Location,
+///     location: snafu::Location,
 /// }
 /// # }
 /// ```
-#[derive(Copy, Clone)]
-#[non_exhaustive]
-pub struct Location {
-    /// The file where the error was reported
-    pub file: &'static str,
-    /// The line where the error was reported
-    pub line: u32,
-    /// The column where the error was reported
-    pub column: u32,
-}
-
-impl Location {
-    /// Constructs a `Location` using the given information
-    pub fn new(file: &'static str, line: u32, column: u32) -> Self {
-        Self { file, line, column }
-    }
-}
-
-impl Default for Location {
-    #[track_caller]
-    fn default() -> Self {
-        let loc = core::panic::Location::caller();
-        Self {
-            file: loc.file(),
-            line: loc.line(),
-            column: loc.column(),
-        }
-    }
-}
+pub type Location = &'static core::panic::Location<'static>;
 
 impl GenerateImplicitData for Location {
     #[inline]
     #[track_caller]
     fn generate() -> Self {
-        Self::default()
-    }
-}
-
-impl fmt::Debug for Location {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Location")
-            .field("file", &self.file)
-            .field("line", &self.line)
-            .field("column", &self.column)
-            .finish()
-    }
-}
-
-impl fmt::Display for Location {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{file}:{line}:{column}",
-            file = self.file,
-            line = self.line,
-            column = self.column,
-        )
+        core::panic::Location::caller()
     }
 }
 
@@ -1569,7 +1517,7 @@ impl fmt::Display for Location {
 #[macro_export]
 macro_rules! location {
     () => {
-        $crate::Location::new(file!(), line!(), column!())
+        core::panic::Location::caller()
     };
 }
 
