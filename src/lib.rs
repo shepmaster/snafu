c0e8e2f90c0d6bd351cc4e05ac38288987c00195
Jake Goulding <jake.goulding@gmail.com>
2026-02-11 14:46:51 -0500

    Clarify what we meant by "inaccurate" and be less negative

--- src/lib.rs
+++ src/lib.rs
@@ -1443,12 +1443,60 @@ fn backtrace_collection_enabled() -> bool {
 ///
 /// ## Limitations
 ///
-/// ### Disabled context selectors
+/// Implicitly generated data, including `Location`, is generated when
+/// the wrapping error value is constructed:
 ///
-/// If you have [disabled the context selector][disabled], SNAFU will
-/// not be able to capture an accurate location.
+/// ```rust
+/// # use snafu::{prelude::*, Location, location};
+/// # fn fallible_code() -> Result<(), InnerError> { Err(InnerError) }
+/// # #[derive(Debug, Snafu)] struct InnerError;
+/// # #[derive(Debug, Snafu)]
+/// # struct InterestingError {
+/// #   source: InnerError,
+/// #   #[snafu(implicit)] location: Location,
+/// # }
+/// # let base_loc = location!();
+/// # let r: Result<(), InterestingError> = (|| {
+/// // The first we know about the error is on this line:
+/// let e = fallible_code();
+/// // but the location will correspond to this line:
+/// e.context(InterestingSnafu)?;
+/// # Ok(())
+/// # })();
+/// # let e = r.unwrap_err();
+/// # assert_eq!(e.location.line(), base_loc.line() + 5);
+/// ```
 ///
-/// As a workaround, re-enable the context selector.
+/// If you have [disabled the context selector][disabled], the
+/// `Location` will correspond to where the `From` implementation is
+/// invoked. This is usually part of the `?` operator:
+///
+/// ```rust
+/// # use snafu::{prelude::*, Location, location};
+/// # fn fallible_code() -> Result<(), InnerError> { Err(InnerError) }
+/// # #[derive(Debug, Snafu)] struct InnerError;
+/// # #[derive(Debug, Snafu)]
+/// # #[snafu(context(false))]
+/// # struct InterestingError {
+/// #   source: InnerError,
+/// #   #[snafu(implicit)] location: Location,
+/// # }
+/// # let base_loc = location!();
+/// # let r: Result<(), InterestingError> = (|| {
+/// // The first we know about the error is on this line:
+/// let e = fallible_code();
+/// // but the location will correspond to this line:
+/// e?;
+/// # Ok(())
+/// # })();
+/// # let e = r.unwrap_err();
+/// # assert_eq!(e.location.line(), base_loc.line() + 5);
+/// ```
+///
+/// Inspecting the code at the generated `Location` will usually
+/// quickly lead back to the original error, but it's recommended to
+/// create the wrapping error as close to the original error location
+/// to reduce confusion.
 ///
 /// [disabled]: Snafu#disabling-the-context-selector
 ///
